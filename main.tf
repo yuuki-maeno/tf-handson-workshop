97033bc481c17c12a00a836600a13d30c0867e23
User <user@Student.local>
2020-01-22 15:54:39 +0900

    second commit

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+
 terraform {
-required_version = "~> 0.12"
+  required_version = "~> 0.12"
 }
+
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region = var.region
+}
+
+resource "aws_instance" "hello-tf-instance" {
+  ami = var.ami
+  count = var.hello_tf_instance_count
+  instance_type = var.hello_tf_instance_type
+}
+
---
+++ variables.tf
@@ -0,0 +1,12 @@
+
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {}
+variable "ami" {}
+variable "hello_tf_instance_count" {
+    default = 2
+}
+variable "hello_tf_instance_type" {
+    default = "t2.micro"
+}
+
